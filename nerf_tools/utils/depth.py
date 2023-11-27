dc3b2e9c5ec4d555a51cd007e59eb6bd63ecf0bc
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2023-11-27 18:04:42 +1000

    Add downsample to avoid out of memory

--- nerf_tools/utils/depth.py
+++ nerf_tools/utils/depth.py
@@ -4,6 +4,10 @@ from nerf_tools.dataset.nerf_dataset import NeRFDataset
 from nerf_tools.dataset.replicaCAD_dataset import ReplicaDataset
 from typing import Union
 
+timing = True
+if timing:
+    import time
+
 def get_pointcloud(dataset: Union[NeRFDataset, ReplicaDataset], max_depth = 10, skip_frames = 1) \
     -> o3d.geometry.PointCloud:
 
@@ -11,7 +15,8 @@ def get_pointcloud(dataset: Union[NeRFDataset, ReplicaDataset], max_depth = 10, 
     camera = dataset.get_camera()    
     for ix, frame in enumerate(dataset.frames):
         if ix % skip_frames == 0:
-
+            if timing:
+                start = time.time()
             rgbd, pose = dataset.sample_o3d(ix, depth_trunc= max_depth)
 
             pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, camera)
@@ -24,8 +29,20 @@ def get_pointcloud(dataset: Union[NeRFDataset, ReplicaDataset], max_depth = 10, 
             pcd_final.colors = o3d.utility.Vector3dVector(
                 np.concatenate([np.asarray(pcd_final.colors),np.asarray(pcd.colors)],
                 axis=0))
-
+            if timing:
+                end = time.time()
+                print(f"Frame {ix} took {end - start} seconds")        
+        # downsample the point cloud 
+        if ix % 5 == 0:
+            if timing:
+                start = time.time()
+            pcd_final = pcd_final.voxel_down_sample(voxel_size=0.04)
+            if timing:
+                end = time.time()
+                print(f"Downsample took {end - start} seconds")
+    pcd_final = pcd_final.voxel_down_sample(voxel_size=0.04)
     return pcd_final
+            
 
 def get_tsdf(dataset: Union[NeRFDataset, ReplicaDataset]):
     volume = o3d.pipelines.integration.ScalableTSDFVolume(
