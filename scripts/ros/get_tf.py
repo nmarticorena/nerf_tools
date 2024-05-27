20a996267f08fac3b409d230ecedc9b086a0097f
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2024-05-27 10:38:37 +1000

    Ruff lsp

--- scripts/ros/get_tf.py
+++ scripts/ros/get_tf.py
@@ -5,6 +5,7 @@ Really simple script that get the 4x4 transform matrix from two tfs
 on ros
 
 """
+
 import rospy
 import tf
 import spatialmath as sm
@@ -17,18 +18,12 @@ t = tf.TransformListener()
 rospy.sleep(0.1)
 while not rospy.is_shutdown():
     try:
-        T = t.lookupTransform("/panda_link0", "/base_link", rospy.Time(0))
+        T = t.lookupTransform("/camera_link", "/panda_hand", rospy.Time(0))
         print(T)
-        T = sm.SE3.Rt(
-            R = smb.q2r(T[1], order="xyzs"),
-            t = T[0],
-            check = False
-        )
+        T = sm.SE3.Rt(R=smb.q2r(T[1], order="xyzs"), t=T[0], check=False)
         T = T.norm()
-        np.savetxt("base_link.txt", T.A)
+        np.savetxt("camera.txt", T.A)
 
         rospy.signal_shutdown("Finished")
-    except Exception  as error:
+    except Exception as error:
         print(error)
-
-
