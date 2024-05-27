e0967d25b05d103ed96dd9354f1e46c2bca6dd2a
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2024-05-27 10:35:38 +1000

    Add configs for generate plots

---
+++ nerf_tools/plots.py
@@ -0,0 +1,32 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def configure_matplotlib():
+    sns.set()
+    sns.set_style(style="whitegrid")
+    sns.set_palette("colorblind", 6)
+    sns.set_context("paper")
+    matplotlib.rcParams["ps.useafm"] = True
+    matplotlib.rcParams["pdf.use14corefonts"] = True
+    matplotlib.rcParams["legend.columnspacing"] = 1.0
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["axes.unicode_minus"] = False
+    plt.rcParams["xtick.major.pad"] = "0"
+    plt.rcParams["ytick.major.pad"] = "0"
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.serif"] = ["Times New Roman"] + plt.rcParams["font.serif"]
+    plt.rcParams.update({"font.size": 10})
+    plt.rcParams.update({"axes.labelsize": 10})
+    matplotlib.rcParams["svg.fonttype"] = "none"
+    # default xlim[]
+    plt.rcParams["axes.xmargin"] = 0
+
+
+def set_one_column(hegiht=3.5):
+    plt.rcParams.update({"figure.figsize": (3.5, hegiht)})
+
+
+def set_two_column(hegiht=3.5):
+    plt.rcParams.update({"figure.figsize": (7, hegiht)})
