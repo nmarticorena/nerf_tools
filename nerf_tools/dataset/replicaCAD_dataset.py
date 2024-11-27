2694533c03fb13bc63b3b08bc7fc55fd212b2cf4
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2024-11-27 15:46:06 +1000

    Heavily modified version of replicaCAD dataset to load them in my structure

--- nerf_tools/dataset/replicaCAD_dataset.py
+++ nerf_tools/dataset/replicaCAD_dataset.py
@@ -8,6 +8,7 @@ import multiprocessing as mp
 import spatialmath as sm
 import trimesh
 import torch
+import open3d as o3d
 
 @dataclass
 class ReplicaFrame:
@@ -33,36 +34,39 @@ class ReplicaDataset:
     frames_index: List[int] = field(default_factory=list)    
     gt_mesh_path : str = "test"
 
-    def __init__(self, config, parent_path = "", load_gt= True, *args, **kwargs):
-        
+    def __init__(self, config, parent_path = "", sequence_name = "",load_gt= True, load_isdf = False, *args, **kwargs):
+        """
+        Config is the json inside the isdf data:
+        Include info of the detph scale, fps, and camera intrinsics
+        """ 
         self.fl_x = config["camera"]["fx"]
         self.fl_y = config["camera"]["fy"]
         self.cx = config["camera"]["cx"]
         self.cy = config["camera"]["cy"]
         self.h = config["camera"]["h"]
         self.w = config["camera"]["w"]
-
-        self.gt_mesh_path = f"{parent_path}" + "/" + config["gt_sdf_dir"] + "mesh.obj"
-
-        self.gt_sdf_path = f"{parent_path}"  + "/" + config["gt_sdf_dir"] + "/1cm/sdf.npy"
-        
+        self.gt_loaded = load_gt
+        self.depth_scale = config["depth_scale"]
+        self.integer_depth_scale = 1/self.depth_scale
+                
         if load_gt:
+            self.gt_mesh_path = f"{parent_path}" + "/" + config["gt_sdf_dir"] + "mesh.obj"
+            self.gt_sdf_path = f"{parent_path}"  + "/" + config["gt_sdf_dir"] + "/1cm/sdf.npy"
             self.gt_sdf = np.load(self.gt_sdf_path)
-        # self.gt_sdf = np.load(self.gt_sdf_path)
-
-        self.gt_stage_sdf_path = f"{parent_path}" + "/" + config["gt_sdf_dir"] + "/1cm/stage_sdf.npy"
-        if load_gt:
+            self.gt_stage_sdf_path = f"{parent_path}" + "/" + config["gt_sdf_dir"] + "/1cm/stage_sdf.npy"
             self.gt_stage_sdf = np.load(self.gt_stage_sdf_path)
-
-        self.frames_index = config["im_indices"]
-        self.folder = f"{parent_path}" + "/" + config["seq_dir"]
+        if load_isdf:
+            self.frames_index = config["im_indices"]
+            self.folder = f"{parent_path}" + "/" + config["seq_dir"]
+        else:
+            self.frames_index = []
+            self.folder = parent_path + "/seqs/" + sequence_name+ "/"
 
         self.load_transforms()
         self.get_frames()
-        # os.makedirs(self.folder, exist_ok=True)
 
     def get_camera(self):
-        return open3d.camera.PinholeCameraIntrinsic(self.w, 
+        return o3d.camera.PinholeCameraIntrinsic(self.w, 
                                              self.h, 
                                              self.fl_x, 
                                              self.fl_y,
@@ -76,9 +80,9 @@ class ReplicaDataset:
         assert idx>=0 and idx<self.n_frames, \
             f"sample index out of range [0,{self.n_frames}]"
         
-        color = open3d.io.read_image(self.image_path[idx])
-        depth = open3d.io.read_image(self.depth_path[idx])
-        rgbd = open3d.geometry.RGBDImage.create_from_color_and_depth(
+        color = o3d.io.read_image(self.image_path[idx])
+        depth = o3d.io.read_image(self.depth_path[idx])
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
             color, depth, depth_scale = 1.0/(self.depth_factor),
             depth_trunc=depth_trunc, convert_rgb_to_intensity=False)
 
@@ -104,31 +108,34 @@ class ReplicaDataset:
 
         bounds_filename = f"{self.folder}bounds.txt"
         bounds = np.loadtxt(bounds_filename)
-
-        mesh = trimesh.exchange.load.load(self.gt_mesh_path, process=False)
-        
-        
-
-
-
+        __import__('pdb').set_trace()
         self.aabb = np.zeros((2,3))
 
-        self.inv_bounds_transform, bounds_extends = trimesh.bounds.oriented_bounds(mesh)
-        self.scene_center = mesh.bounds.mean(axis = 0)
 
-        # self.inv_bounds_transform = torch.from_numpy(
-        #     T_extent_to_scene).float().to('cuda')
-        self.bounds_transform_np = np.linalg.inv(self.inv_bounds_transform)
-        # self.bounds_transform = torch.from_numpy(
-        #     self.bounds_transform_np).float().to('cuda')
+        if self.gt_loaded:
+
+            mesh = trimesh.exchange.load.load(self.gt_mesh_path, process=False)
+        
+        
 
 
 
-        self.aabb[0,:] = mesh.vertices.min(axis = 0)
-        self.aabb[1,:] = mesh.vertices.max(axis = 0)
+
+            self.inv_bounds_transform, bounds_extends = trimesh.bounds.oriented_bounds(mesh)
+            self.scene_center = mesh.bounds.mean(axis = 0)
+
+            # self.inv_bounds_transform = torch.from_numpy(
+            #     T_extent_to_scene).float().to('cuda')
+            self.bounds_transform_np = np.linalg.inv(self.inv_bounds_transform)
+            # self.bounds_transform = torch.from_numpy(
+            #     self.bounds_transform_np).float().to('cuda')
+
+
+
+            self.aabb[0,:] = mesh.vertices.min(axis = 0)
+            self.aabb[1,:] = mesh.vertices.max(axis = 0)
 
         self.transforms  = np.loadtxt(filename).reshape(-1,4,4)
-
         # grid_range = [-1.0, 1.0]
         # range_dist = grid_range[1] - grid_range[0]
         # self.scene_scale_np = bounds_extends / (range_dist * 0.9)
@@ -140,13 +147,39 @@ class ReplicaDataset:
 
     def get_frames(self):
         self.frames = []
-        for i in self.frames_index:
-            frame = ReplicaFrame()
-            # frame.rgb = cv2.imread(f"{self.gt_mesh_path}/rgb_{i:04d}.png")
-            # frame.depth = cv2.imread(f"{self.gt_mesh_path}/depth_{i:04d}.png", cv2.IMREAD_ANYDEPTH)
-            # frame.noise_depth = cv2.imread(f"{self.gt_mesh_path}/noise_depth_{i:04d}.png", cv2.IMREAD_ANYDEPTH)
-            frame.transform_matrix = self.transforms[i,:,:]
-            self.frames.append(frame)
+        if self.frames_index == []:
+            i = 0
+            skip = 20
+            with open(f"{self.folder}/associations.txt", 'r') as f:
+                for l in f:
+                    frame_json = {}
+                    if i % skip == 0:
+                        frame = ReplicaFrame()
+                        info = l.split()
+                        rgb_path = info[1]
+                        depth_path = info[3]
+
+
+                        # frame.rgb = cv2.imread(f"{self.folder}/{info[1]}")
+                        # frame.depth = cv2.imread(f"{self.folder}/{info[3]}", cv2.IMREAD_ANYDEPTH)
+                        frame.transform_matrix = self.transforms[i,:,:] @ sm.SE3.Rx(np.pi).A
+
+                        frame_json["file_path"] = rgb_path
+
+                        # Need to convert from 
+
+                        frame_json["transform_matrix"] = frame.transform_matrix.tolist()
+                        frame_json["depth_path"] = depth_path
+                        self.frames.append(frame_json)
+                    i += 1
+        else:
+            for i in self.frames_index:
+                frame = ReplicaFrame()
+                frame.rgb = cv2.imread(f"{self.folder}/results/rgb_{i:04d}.png")
+                frame.depth = cv2.imread(f"{self.folder}/results/depth_{i:04d}.png", cv2.IMREAD_ANYDEPTH)
+                frame.noise_depth = cv2.imread(f"{self.folder}/results/noise_depth_{i:04d}.png", cv2.IMREAD_ANYDEPTH)
+                frame.transform_matrix = self.transforms[i,:,:]
+                self.frames.append(frame)
     
     def get_transforms(self, indexs):
         return [np.array(self.transforms[i]) for i in indexs]
@@ -188,11 +221,8 @@ class ReplicaDataset:
 
 # Test
 if __name__ == "__main__":
-    import neo_sddf.dependencies.iSDF as iSDF
-    import inspect
 
-
-    parent_path = "/home/nmarticorena/Documents/papers/neo-sddf/neo_sddf/dependencies/iSDF/"
+    parent_path = "/media/nmarticorena/iSDF_data/"
     import json
     with open(f"{parent_path}/results/iSDF/apt_3/0/config.json", 'r') as f:
         config = json.load(f)
